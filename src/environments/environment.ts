8f947df0dda90c59973525a853656abcd1b90524
Bryson69 <brysonmundia@gmail.com>
2020-06-23 01:44:41 +0300

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apikey: '0fd127907c8c783d11161c3fd7678b3cc25a3115'
 };
 
 /*
