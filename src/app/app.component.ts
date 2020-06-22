3c8602579e972d288c33a6e79b09e1f4b078159b
Bryson69 <brysonmundia@gmail.com>
2020-06-23 01:43:43 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:`
+  <app-homepage></app-homepage>
+  `
 })
 export class AppComponent {
   title = 'githubhub';
