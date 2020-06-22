a3cffbb66f744b2106356c14d2c6b54a4ea4ca39
Bryson69 <brysonmundia@gmail.com>
2020-06-23 01:44:01 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+// import { DateCountPipe } from './date-count.pipe';
+import { GitService } from './git.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  declarations: [ AppComponent, HomepageComponent ],
+  imports: [ BrowserModule, AppRoutingModule, HttpClientModule, FormsModule ],
+  providers: [GitService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
